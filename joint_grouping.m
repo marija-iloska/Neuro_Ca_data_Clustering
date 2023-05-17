8c5015c6b0f5afd19f0c255c9f2efa7c2be8ced2
marija-iloska <marija.iloska@stonybrook.edu>
2023-05-17 15:41:54 -0400

    small update

--- joint_grouping.m
+++ joint_grouping.m
@@ -13,9 +13,35 @@ for n = 1:num_neurons
     area_salt(n) =  trapz(abs(dFoF_salt(n,:)));
     area_inter(n) =  trapz(abs(dFoF_inter(n,:)));
 
-    % Sort area and get indices
-    [areaSU, idx_SU] = sort(area_sugar, 'descend');
-    [areaSA, idx_SA] = sort(area_salt, 'descend');
-    [areaIN, idx_IN] = sort(area_inter, 'descend');
 end
+
+% Sort area and get indices
+[areaSU, idx_SU] = sort(area_sugar, 'descend');
+[areaSA, idx_SA] = sort(area_salt, 'descend');
+[areaIN, idx_IN] = sort(area_inter, 'descend');
+
+% Get dF/F for top N most active
+N = 10;
+F_SU = dFoF_sugar(idx_SU(1:N), :);
+F_SA = dFoF_salt(idx_SA(1:N), :);
+F_IN = dFoF_inter(idx_IN(1:N), :);
+
+
+% Specify the number of clusters you want to obtain
+k = 10;
+
+% Compute the distance matrix between the rows of F
+%dist_mat = pdist(F);
+dist_mat = pdist2(F_SU, F_SU);
+
+% Perform hierarchical clustering using complete linkage
+Z = linkage(dist_mat, 'average');
+
+% Extract the cluster assignments from the hierarchical tree
+idx = cluster(Z, 'Maxclust', k);
+
+% Plot a dendrogram of the hierarchical clustering result
+figure(n)
+dendrogram(Z, 0);
+set(gca, 'FontSize', 15)
 
