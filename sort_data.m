88fe50a263659b9260fe4d2f222e10216b91c0a0
marija-iloska <50119390+marija-iloska@users.noreply.github.com>
2023-10-10 12:11:41 -0400

    cleaned up folder and wrote efficient data sorting
    Extracts PSTHs
    index frames
    First and second halfs of trials
    Entire trials
    Salt and Sugar

--- sort_data.m
+++ sort_data.m
@@ -7,8 +7,8 @@ load Ca_data.mat
 
 % Tastes in experiments
 intervals = {'sugar', 'salt'};
-split = {'first', 'second'};
-choice = split{1};
+split = {'entire', 'first', 'second'};
+
 
 % Number of tastes in experiments
 num_tastes = length(intervals);
@@ -19,6 +19,12 @@ num_trials = [num_sugar_trials, num_salt_trials];
 % Store frame indices for each trail
 trial_frames = {sugar_start_end, salt_start_end};
 
+% Normalizing sum
+min_F = min(min(dFoF));
+w_norm = sum(dFoF - min_F, 2);
+dFoF_norm = (dFoF -min_F)./w_norm;
+dFoF_norm = dFoF_norm/(max(max(dFoF_norm)));
+
 
 % JOIN all "taste" trials
 for i = 1 : num_tastes
@@ -26,47 +32,70 @@ for i = 1 : num_tastes
     % Which taste
     trial = trial_frames{i};
     range = [];
-    inter = [];
+    first = [];
+    second = [];
+    psth_entire = [];
+    psth_first  = [];
+    psth_second = [];
 
-    % Collect every trial index range
-    for n = 2: num_trials(i)   
-        temp =  (trial{n}(1) + floor( 0.5*(trial{n}(2) - trial{n}(1)))) : trial{n}(2);
-        %temp = trial{n}(1) : (trial{n}(1) + floor( 0.5*(trial{n}(2) - trial{n}(1))))
-        %temp = trial{n}(1) : trial{n}(2);
-        range = [range temp];
-        % temp_inter = trial{n-1}(end)+1 : trial{n}(1)-1;
-        temp_inter = (trial{n-1}(end)+1 + floor( 0.5*(trial{n}(1)-1 - trial{n-1}(end)+1) )) : trial{n}(1)-1; 
-        inter = [inter, temp_inter];
+    % Collect every trial index range - ENTIRE trial range
+    for n = 1: num_trials(i)   
+
+        % Extract range
+        temp =  trial{n}(1): trial{n}(2);
+        first_temp = temp(1) : floor(0.5*(temp(1) + temp(end)));
+        second_temp = setdiff(temp, first_temp);
         
+
+        % Concatenate with all previous extracted ranges
+        range = [range temp];  
+        first = [first first_temp];
+        second = [second second_temp];
+        
+        % PSTH
+        psth_entire = [psth_entire;   dFoF_norm(:,temp)];
+        psth_first  = [psth_first;    dFoF_norm(:,first_temp)];
+        psth_second = [psth_second;   dFoF_norm(:,second_temp)];
     end
 
-    % Get JOINT dFoF
-%     first_itrial = 1:trial{1}(1)-1;
-%     last_itrial = trial{num_trials(i)}(end)+1 :T;
-%     inter = [first_itrial, inter, last_itrial];  
-    str = join([ choice, '_inter_', intervals{i}]);
-    assignin('base',str, inter);
+    % Get JOINT dFoF ranges
+    entire_trial{i} = range;
+    first_half{i} = first;
+    second_half{i} = second;
 
-    % Get JOINT dFoF
-    %range = [trial{1}(1) : trial{1}(2), range];
-    range = [trial{1}(1) : (trial{1}(1) + floor(0.5*(trial{1}(2) - trial{1}(1)))), range];
-    str = join(['dFoF_', choice, '_', intervals{i}]);
-    assignin('base',str, dFoF(:, range));
+    % PSTH
+    psth_trial{i} = squeeze(mean(psth_entire, 1));
+    psth_taste{i} = squeeze(mean(psth_first, 1));
+    psth_decis{i} = squeeze(mean(psth_second, 1));
+
+
+    % Name and get values using indices
+    str = join(['dFoF_entire', '_', intervals{i}]);
+    assignin('base',str, dFoF(:, entire_trial{i}));
+
+    str = join(['dFoF_first', '_', intervals{i}]);
+    assignin('base',str, dFoF(:, first_half{i}));
+
+    str = join(['dFoF_second', '_', intervals{i}]);
+    assignin('base',str, dFoF(:, second_half{i}));
+
 end
 
-% add inter trial
-intervals = {'sugar', 'salt', 'inter'};
-frames_inter = intersect(first_inter_salt, first_inter_sugar);
-%frames_inter = intersect(second_inter_salt, second_inter_sugar);
 
-str = join(['dFoF_', choice, '_', intervals{3}]);
-assignin('base',str, dFoF(:, frames_inter));
+% Get joint inter trial frames
+inter_trial = setdiff(1:T, [entire_trial{1}, entire_trial{2}]);
 
-dFoF_first = {dFoF_first_sugar, dFoF_first_salt, dFoF_first_inter};
-%dFoF_second = {dFoF_second_sugar,dFoF_second_salt, dFoF_second_inter};
+str = 'dFoF_inter';
+assignin('base',str, dFoF(:, inter_trial));
 
 
-%save('trial_data.mat', 'dFoF_inter', 'dFoF_sugar', 'dFoF_salt', 'frames_inter', 'intervals')
-%save('trial_data_first.mat', 'dFoF_first', 'dFoF_first_inter', 'dFoF_first_sugar', 'dFoF_first_salt', 'intervals')
-%save('trial_data_second.mat', 'dFoF_second','dFoF_second_inter', 'dFoF_second_sugar', 'dFoF_second_salt', 'intervals')
+% Save variables
+frames = {entire_trial, first_half, second_half, inter_trial};
+dFoF_sugar = {dFoF_entire_sugar, dFoF_first_sugar, dFoF_second_sugar};
+dFoF_salt = {dFoF_entire_salt, dFoF_first_salt, dFoF_second_salt};
+
+% Save file
+save('trial_data.mat', 'dFoF_sugar', 'dFoF_salt', 'frames', 'split', ...
+    'dFoF_norm', 'psth_decis', 'psth_taste', 'psth_trial')
+
 
